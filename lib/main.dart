e6302790f5e06ecaecee25a0d1af16c058161d59
salaz310 <lrso.10@hotmail.com>
2019-09-10 09:29:22 -0600

    Prueba Codemagic

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'My Movie',
+      title: 'PRUEBA CODEMAGIC',
 
       //Ruta inicial
       initialRoute: 'scroll',
